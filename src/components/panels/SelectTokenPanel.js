242c08567d44247a99d4a771a5f863b88502ef6b
Diana Maduan <dianamaduan@MacBook-Air-Diana.local>
2021-11-03 19:33:25 +0600

    fix token list loader

--- src/components/panels/SelectTokenPanel.js
+++ src/components/panels/SelectTokenPanel.js
@@ -3,22 +3,27 @@ import { Panel,Search } from '@vkontakte/vkui';
 import Header from '../uikit/Header'
 import {connect} from 'react-redux';
 import TokenItem from '../uikit/TokenItem'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import {sortList} from '../helpers'
 import Loader from '../uikit/Loader'
 const SelectTokenPanel = (props) => {
     const {tokenList} = props.walletReducer
     const [list,setList] = useState(sortList(tokenList))
+    const [loader, setLoader] = useState(tokenList.length > 10)
     const searchToken = (token) => {
         let arr = tokenList.filter(item => item.symbol.substr(0,token.length).toLowerCase() === token.toLowerCase() || item.name.substr(0,token.length).toLowerCase() === token.toLowerCase())
         setList(sortList(arr))
         if(token.length < 1) setList(sortList(tokenList))
     }
+    useEffect(()=>{
+        setLoader(tokenList.length > 10)
+        setList(sortList(tokenList))
+    },[loader,tokenList,list])
     return(
         <Panel id={ROUTES.SELECT_ADDRESS}>
             <Header title="Select token" showTitle={true}  back={true} />
             <Search after={null} placeholder='Search' onChange={(e) => searchToken(e.target.value)}/>  
-            {list.length ? list.map(item => (
+            {loader ? list.map(item => (
                 <TokenItem item={item} withAmount={true} key={item.symbol}/>
             )):
             <Loader id='centered-loader'/>}
