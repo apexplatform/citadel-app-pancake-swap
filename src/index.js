2ed076fd2f70053456f477aec3e6f710bd98ce66
Diana Maduan <dianamaduan@MacBook-Air-Diana.local>
2021-11-22 18:00:12 +0600

    added sentry

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from "react";
 import ReactDOM from "react-dom";
 import bridge from "@vkontakte/vk-bridge";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import App from "./App";
 import store from './store/store'
 import {Provider} from 'react-redux';
 // Init VK  Mini App
 bridge.send("VKWebAppInit");
-
+Sentry.init({
+  dsn: "https://acbfd8c16a824dcf929ce1d6cf623f04@o510489.ingest.sentry.io/6075406",
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>
   , document.getElementById("root"));
-if (process.env.NODE_ENV === "development") {
-  import("./eruda").then(({ default: eruda }) => {}); //runtime download
-}
+
