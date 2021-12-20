d5a7fc49095c35aa3f7655a17a35347f1a0d76d9
Diana Maduan <dianamaduan@MacBook-Air-Diana.local>
2021-12-20 14:57:39 +0600

    fix error modal

--- src/components/uikit/ErrorModal.js
+++ src/components/uikit/ErrorModal.js
@@ -5,8 +5,13 @@ import '../styles/components/errorModal.css'
 import text from '../../text.json'
 const ErrorModal = (props) => {
     const {networkErrors,validationErrors} = props.errorsReducer
+    const closeModal = () => {
+        if(networkErrors?.text) {
+            props.setActiveModal(null)
+        }
+    }
     return(
-		<ModalPage id="errors" dynamicContentHeight>
+		<ModalPage id="errors" dynamicContentHeight onClose={() =>  closeModal()}>
             <div id='modal-header'>
             {
             !validationErrors?.header ? 
@@ -20,13 +25,15 @@ const ErrorModal = (props) => {
             </div>
                 <p className='error-text'>{networkErrors?.text || validationErrors?.text} <span>{validationErrors?.text && 'Citadel.one'}</span></p>
                 <p className='error-description'>{networkErrors?.description || validationErrors?.description}</p>
-                <div className='error-tips'>
-            <img src='/img/icons/tips.svg' alt='error' />
+            <div className='error-tips' id={networkErrors?.text ? 'error-tips' : undefined}>
+                <img src='/img/icons/tips.svg' alt='error' />
                 <div>
                     <h4>{text.TIP}</h4>
                     <span className='tips-description'>{networkErrors?.tip || validationErrors?.tip}</span>
                 </div>
             </div>
+            {networkErrors?.text ?
+            <button className='error-btn' id={validationErrors?.header ? 'purple-btn' : undefined} onClick={() => props.setActiveModal(null)}>Ok</button> : ''}
 		</ModalPage>
 	  );
 }
