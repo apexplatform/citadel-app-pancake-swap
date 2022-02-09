b9fec34bb1c0724716dfde64f7393bbb6434da4a
maduandiana <maduandiana@gmail.com>
2022-02-09 16:24:32 +0600

    fix token

--- src/store/reducers/userReducer.js
+++ src/store/reducers/userReducer.js
@@ -1,9 +1,8 @@
 const qs = require('querystring');
 const params = window.location.search.slice(1);
 const paramsAsObject = qs.parse(params);
-const token = JSON.parse(paramsAsObject.token);
 const initialState = {
-    auth_token: token?.token || token
+    auth_token: paramsAsObject?.token
 }
 export default function(state=initialState,action){
     switch (action.type){
