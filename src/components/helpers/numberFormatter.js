472d6e430eb488de4aff3bbc4727be175473da04
maduandiana <maduandiana@gmail.com>
2022-02-10 20:28:54 +0600

    fix separator

--- src/components/helpers/numberFormatter.js
+++ src/components/helpers/numberFormatter.js
@@ -55,15 +55,18 @@ export const formatNumber = (amount) => {
   const intval = +decval !== 0 ? new IntPretty(decval).trim(true).maxDecimals(3).toString() : decval
   return intval.toString().replaceAll(',','')
 }
-
 export function numberWithCommas(x,decimals) {
-  if(+x==0){
-    return x
+  if(x){
+    if(+x==0){
+      return x
+    }
+    let numFixed = decimals ? x?.toFixed(decimals) : x
+    if(numFixed == '0.00'){
+      return '~0'
+    }
+    var str = numFixed?.toString().split(".");
+    str[0] = str[0]?.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return str.join(".");
   }
-  let numFexed = decimals ? x?.toFixed(decimals) : x
-  if(numFexed == '0.00'){
-    return '~0'
-  }
-  let num = numFexed?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-  return num
+  return x
 }
--- src/networking/hooks/allowanceHooks.js
+++ src/networking/hooks/allowanceHooks.js
@@ -1,7 +1,7 @@
 import { useTokenContract } from './contractHooks'
 import {SPENDER} from '../constants/constants'
-export const loadTokenAllowance = async(token=null,currentWallet,fromToken) =>  {
-  const contract = useTokenContract(token?.address || fromToken?.address)
+export const loadTokenAllowance = async(token,currentWallet) =>  {
+  const contract = useTokenContract(token?.address)
   const response = await contract?.allowance(currentWallet?.address, SPENDER)
   return parseInt(response?._hex || '0x0', 16)
 }
