9251f84500a5500147a0428438a02e506ff82cdc
maduandiana <maduandiana@gmail.com>
2022-03-11 14:09:30 +0600

    fix url

--- src/networking/utils/getRpcUrl.ts
+++ src/networking/utils/getRpcUrl.ts
@@ -7,9 +7,6 @@ if (!process.env.REACT_APP_NODE_1 || !process.env.REACT_APP_NODE_2 || !process.e
 // Array of available nodes to connect to
 export const nodes = [process.env.REACT_APP_NODE_1, process.env.REACT_APP_NODE_2, process.env.REACT_APP_NODE_3]
 
-if (process.env.NODE_ENV === 'production' && process.env.REACT_APP_NODE_ALT) {
-  nodes.push(process.env.REACT_APP_NODE_ALT)
-}
 
 const getNodeUrl = () => {
   return sample(nodes)
