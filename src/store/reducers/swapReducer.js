b21ecfa465d49722567626eaec90417ebe5bd493
Diana Maduan <maduandiana@gmail.com>
2022-07-12 10:37:08 +0600

    fix bugs

--- src/store/reducers/swapReducer.js
+++ src/store/reducers/swapReducer.js
@@ -19,12 +19,12 @@ import { types } from "../actions/types";
     selectedToken: 'INPUT',
     routes: [
       {
-          code: 'cosmos',
-          network: 'Cosmos'
+        logoURI: 'https://tokens.pancakeswap.finance/images/0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d.png',
+        name: 'USDC'
       },
       {
-          code: 'osmosis',
-          network: 'Osmosis'
+        logoURI: 'https://tokens.pancakeswap.finance/images/0x55d398326f99059fF775485246999027B3197955.png',
+        name: 'BSC'
       }
     ]
   };
