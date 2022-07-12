896010ceb59dacc76b504d86f30500789e70ae0a
Diana Maduan <maduandiana@gmail.com>
2022-07-12 16:56:48 +0600

    guides as component

--- src/components/panels/GuidesPanel.js
+++ src/components/panels/GuidesPanel.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { GuidesCard } from '@citadeldao/apps-ui-kit/dist/main'
 import text from '../../text.json'
-import guide1 from '../guides/guide1.svg'
-import guide2 from '../guides/guide2.svg'
-import guide3 from '../guides/guide3.svg'
-import guide4 from '../guides/guide4.svg'
-import guide5 from '../guides/guide5.svg'
+import '../styles/uiKit/guides.css'
+import { ReactComponent as Guide1 } from '../guides/guide1.svg'
+import { ReactComponent as Guide2 } from '../guides/guide2.svg'
+import { ReactComponent as Guide3 } from '../guides/guide3.svg'
+import { ReactComponent as Guide4 } from '../guides/guide4.svg'
+import { ReactComponent as Guide5 } from '../guides/guide5.svg'
 const GuidesPanel = () => {
     return (
         <div className='guides-panel'>
@@ -19,29 +20,29 @@ const GuidesPanel = () => {
             <GuidesCard text={text.GUIDES_HEADER_2}>
                 <div className='row guide-content-1'>
                     <p>{text.GUIDES_DESCRIPTION_2}</p>
-                    <img src={guide1} alt='quide1' />
+                    <Guide1 className='quide-img'/>
                 </div>
             </GuidesCard>
             <GuidesCard text={text.GUIDES_HEADER_3}>
                 <div className='row guide-content-2'>
                     <p>{text.GUIDES_DESCRIPTION_3}</p>
-                    <img src={guide2} alt='quide1' />
+                    <Guide2 className='quide-img' />
                 </div>
                 <div className='row guide-content-2'>
                     <p>{text.GUIDES_DESCRIPTION_3_2}</p>
-                    <img src={guide3} alt='quide1' />
+                    <Guide3 className='quide-img' />
                 </div>
             </GuidesCard>
             <GuidesCard text={text.GUIDES_HEADER_4}>
                 <div className='row guide-content-3'>
                     <p>{text.GUIDES_DESCRIPTION_4}</p>
-                    <img src={guide4} alt='quide1' />
+                    <Guide4 className='quide-img' />
                 </div>
             </GuidesCard>
             <GuidesCard text={text.GUIDES_HEADER_5}>
                 <div className='row guide-content-4'>
                     <p>{text.GUIDES_DESCRIPTION_5}</p>
-                    <img src={guide5} alt='quide1' />
+                    <Guide5 className='quide-img' />
                 </div>
             </GuidesCard>
         </div>
