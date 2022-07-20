bbff9043956067562e4d5850b00a53a427f50130
Diana Maduan <maduandiana@gmail.com>
2022-07-20 12:05:22 +0600

    fix sorting

--- src/components/helpers/index.js
+++ src/components/helpers/index.js
@@ -1,7 +1,8 @@
 export const sortList = (list) => {
   let sortedList = list.sort(function(a,b) {
-    // eslint-disable-next-line 
-    if(a.balance == b.balance)
+    if(a.balance === '~0') return 1
+    if(b.balance === '~0') return 1
+    if(a.balance === b.balance)
     {
       return (a.name < b.name) ? -1 : (a.name > b.name) ? 1 : 0;
     }
